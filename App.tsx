1cc6606868c10ffaebe9862c7e90238ecc9b64eb
Thábata Orbeteli <rm550268@fiap.com.br>
2024-04-12 18:14:29 -0300

    update app

--- App.tsx
+++ App.tsx
@@ -1,18 +1,13 @@
-// Importando React e os componentes necessários do Native Base
 import React from "react";
 import { NativeBaseProvider, View } from 'native-base';
 
-// Importando o componente AdicionarTarefa e o ProvedorEstadoGlobal
 import AdicionarTarefa from "./src/components/AdicionarTarefa";
 import { ProvedorEstadoGlobal } from "./src/hooks/EstadoGlobal";
 
-// Importando o componente ListaTarefas
 import ListaTarefas from "./src/components/ListaTarefas";
 
-// Função principal do componente App
 export default function App() {
   return (
-    // Início do contexto do NativeBaseProvider
     <NativeBaseProvider>
       {/* Início do contexto do ProvedorEstadoGlobal */}
       <ProvedorEstadoGlobal>
